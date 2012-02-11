105fdc6ff51a7e0f93943873af6d33c7521258c5
Aditya Rastogi <aditya.rstgi@gmail.com>
2012-02-11 21:46:31 +0530

    -Created the layout for the row -Added the default image resource

--- ListBuster/src/com/adi/ListBuster/ListBusterActivity.java
+++ ListBuster/src/com/adi/ListBuster/ListBusterActivity.java
@@ -1,9 +1,9 @@
 package com.adi.ListBuster;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class ListBusterActivity extends Activity {
+public class ListBusterActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
