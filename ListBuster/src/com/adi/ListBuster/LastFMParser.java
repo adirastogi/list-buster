926237a546a9737b16e2902922b31c4e818cf2e5
Aditya Rastogi <aditya.rstgi@gmail.com>
2012-02-17 19:51:53 +0530

    -created parser class for lastFM

---
+++ ListBuster/src/com/adi/ListBuster/LastFMParser.java
@@ -0,0 +1,11 @@
+package com.adi.ListBuster;
+
+public class LastFMParser implements Parser {
+
+	public SongObject parseResponse(String response) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+}
