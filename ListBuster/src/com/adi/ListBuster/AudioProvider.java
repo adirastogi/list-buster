0434e14ae6cc591db62b18f6bbdd848f5e85a67a
Aditya Rastogi <aditya.rstgi@gmail.com>
2012-02-20 19:46:22 +0530

    -added the accessor methods to the AudioProvider class for convenient access to the formatted values of the cursor.

--- ListBuster/src/com/adi/ListBuster/AudioProvider.java
+++ ListBuster/src/com/adi/ListBuster/AudioProvider.java
@@ -6,23 +6,26 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 
+
 public class AudioProvider {
     
     public static final int HAS_RESULTS=1;
     public static final int IS_NULL=2;
     public static final int CURSOR_ERROR=3;
+    private static Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
     
+    private Context appContext;		//for storing the application context	
+    private Cursor songCursor; 		//this variable holds the present position of the cursor
     
-    private Context appContext;
-    private Cursor songCursor;
-    private Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
     
     public AudioProvider(Context ctxt) {
         appContext = ctxt;
         
     }
-    
-    public Cursor getSongCursor(){
+    /*This method is responsible for querying the media store to get the cursor object
+     * that contains the rows for all the songs.
+    */
+    public Cursor getSongsCursor(){
         
         
         String[] projection = {
@@ -38,7 +41,10 @@ public class AudioProvider {
         
         return songCursor;
     }
-   
+ 
+    /**Returns the albumart for the song record currently pointed to by the cursor.
+     * @return Uri for the albumart resource
+     */
     public Uri getAlbumArtUri() {
         
         if(songCursor==null)
@@ -50,5 +56,51 @@ public class AudioProvider {
         Uri artUri = ContentUris.withAppendedId(authority, albumID);
         return artUri;
     }
+    
+    /**Returns the albumart for the song record currently pointed to by the cursor.
+     * @return the string for songname
+     */
+    public String getSongName(){
+    	
+    	if(songCursor==null)
+    		return null;
+    	
+    	int songNameIndex = songCursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME);
+    	String songName = songCursor.getString(songNameIndex);
+    	return songName;
+    }
+    
+    /**Returns the artistname for the song record currently pointed to by the cursor.
+     * @return string artistname
+     */
+    public String getArtistName(){
+    	
+    	if(songCursor==null)
+    		return null;
+    	
+    	int artistNameIndex = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+    	String artistName = songCursor.getString(artistNameIndex);
+    	return artistName;
+    }
    
+    /**Returns the albumname for the song record currently pointed to by the cursor.
+     * @return string albumname
+     */
+    public String getAlbumName(){
+    	
+    	if(songCursor==null)
+    		return null;
+    	
+    	int albumNameIndex = songCursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+    	String albumName = songCursor.getString(albumNameIndex);
+    	return albumName;
+    }
+    
+    /**basically calls the moveToNext() of cursor.
+     * @return true if the cursor is incremented to a valid record, false otherwise
+     */
+    public boolean nextSong(){
+    	
+    	return songCursor.moveToNext();
+    }
 }
