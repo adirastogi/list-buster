d034f6b77297b3a32727b188910e219d2692e5d8
Aditya Rastogi <aditya.rstgi@gmail.com>
2012-02-22 12:47:40 +0530

    -refactored the class MusicAdapter to use a cursor adapter in place of an ArrayAdapter and overrode the bindView and newView methods. -used AudioProvider utility wrapper class's methods to access the cursor information inside the bindView and newView methods.

--- ListBuster/src/com/adi/ListBuster/MusicAdapter.java
+++ ListBuster/src/com/adi/ListBuster/MusicAdapter.java
@@ -1,55 +1,55 @@
 package com.adi.ListBuster;
 
-import java.util.List;
-
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.CursorAdapter;
 
-public class MusicAdapter extends ArrayAdapter<SongObject>{
+public class MusicAdapter extends CursorAdapter{
 
-    private Context context;
-    private List<SongObject> songList;
     
-    /* (non-Javadoc)
-     * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
-     */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        
-        RowHolder holder;
-        View rowView = convertView;
-   
-        
-        if(rowView==null) {
-            LayoutInflater inflator = LayoutInflater.from(context);
-            rowView = inflator.inflate(R.layout.row,parent);
-            holder = new RowHolder(rowView);
-            rowView.setTag(holder);
-        }
-        else {
-            holder = (RowHolder)rowView.getTag();
-        }
-        
-
-        holder.getArtistName().setText(songList.get(position).getArtist());
-        holder.getSongName().setText(songList.get(position).getSong());
-        holder.getAlbumArt().setImageURI(songList.get(position).getImagepath()); 
-        
-        return rowView;
-        
+    private AudioProvider ap;
+    
+    public MusicAdapter(Context context, AudioProvider ap) {
+    	// TODO Auto-generated constructor stub
+    	super(context,ap.getSongsCursor());
+        this.ap = ap;
     }
 
-    public MusicAdapter(Context context, int textViewResourceId, List<SongObject> objects) {
-        super(context, textViewResourceId, objects);
-        // TODO Auto-generated constructor stub
-        this.context = context;
-        this.songList = objects;
-    }
+	@Override
+	public void bindView(View view, Context context, Cursor songCursor) {
+		// TODO Auto-generated method stub
+		
+        RowHolder holder = (RowHolder)view.getTag();
+        
+        /*We access the methods of theutility class AudioProvider to 
+         * get the formatted information from the cusor since the AudiProvider
+         * object apparently holds a reference to the same cursor object as is 
+         * received in this method.
+         * TODO fix this.
+         */
+
+        holder.getArtistName().setText(ap.getArtistName());
+        holder.getSongName().setText(ap.getSongName());
+        holder.getAlbumArt().setImageURI(ap.getAlbumArtUri()); 
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		 RowHolder holder;
+	     View rowView = null;
+	      
+	     LayoutInflater inflator = LayoutInflater.from(context);
+	     rowView = inflator.inflate(R.layout.row,parent);
+	     holder = new RowHolder(rowView);
+	     rowView.setTag(holder);
+	       	        
+	     return rowView;
+
+	}
     
     
 
